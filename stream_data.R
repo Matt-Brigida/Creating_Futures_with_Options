210a12fe19892b26245ba90ca6e2b50b654d944f
Matt Brigida <mdbrigida@gmail.com>
2019-10-15 15:11:20 -0400

    initial commit

---
+++ stream_data.R
@@ -0,0 +1,23 @@
+library(IBrokers)
+
+### run the following in 3 separate R processes
+
+## pull futures
+con1 <- twsConnect(1)
+
+future <- twsFuture(symbol = "CL", exch = "NYMEX", expiry = "201912")
+reqMktDepth(con1, future, file='future.dat')
+
+
+## pull call option
+con2 <- twsConnect(2)
+
+call <- twsFOP(symbol='CL',exch='NYMEX',expiry='20191115', right = 'C', strike = '53', multiplier = '1000') # LOZ9 C5300
+reqMktDepth(con2, call, file='call.dat')
+
+
+## pull put option
+con3 <- twsConnect(3)
+
+put <- twsFOP(symbol='CL',exch='NYMEX',expiry='20191115', right = 'P', strike = '53', multiplier = '1000') # LOZ9 P5300
+reqMktDepth(con3, put, file='put.dat')
